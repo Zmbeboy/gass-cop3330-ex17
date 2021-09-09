1ccf225d195345e9c09cd0e2b5586d5651749082
Zimbee <61887028+Zmbeboy@users.noreply.github.com>
2021-09-09 18:21:04 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,39 @@
+  
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Eric Gass
+ */
+
+package org.example;
+import java.util.Scanner;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter a 1 if you are male or a 2 if you are female: ");
+        String genderS = scan.nextLine();
+        System.out.print("How many ounces of alcohol did you have? ");
+        String ouncesS = scan.nextLine();
+        System.out.print("What is your weight, in pounds? ");
+        String weightS = scan.nextLine();
+        System.out.print("How many hours has it been since your last drink? ");
+        String hourS = scan.nextLine();
+
+        int gender = Integer.parseInt(genderS);
+        int ounces = Integer.parseInt(ouncesS);
+        int weight = Integer.parseInt(weightS);
+        int hour = Integer.parseInt(hourS);
+
+        double alcDis = (gender == 1) ? 0.73 : 0.66;
+
+        double BAC = (ounces * 5.14 / weight * alcDis) -0.015 * hour;
+
+        System.out.println(String.format("Your BAC is %f",BAC));
+
+        String result = (BAC < 0.08) ? "It is legal for you to drive." : "It is not legal for you to drive.";
+
+        System.out.println(result);
+    }
+}
